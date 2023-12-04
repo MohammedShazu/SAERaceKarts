f4490a342e4182d7f90b677a02c2dbc10c931c88
MohammedShazu <mohammedshazu3@gmail.com>
2023-12-04 17:15:10 +0000

    Initial commit of finish script updated

--- Assets/Finish.cs
+++ Assets/Finish.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
-{
+    { 
+    private int currentLap;
+    private void Start()
+    {
+    currentLap = 1;
+    }
     private void OnTriggerEnter(Collider other)
     {
         // Check if the object entering the trigger is the player's car
@@ -10,6 +16,11 @@ public class Finish : MonoBehaviour
         {
             Debug.Log("Player has reached the finish line!");
             // This is where you can add a finish line for example displaying a victory screen.
+        }
+        if (currentLap == 3)
+        {
+            Debug.Log("Player has finished the race!!!");
+            SceneManager.LoadScene("FinishScene");
         }
     }
 }
