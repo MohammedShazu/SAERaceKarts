160328aced193fb5eaea3396f13096a059f5a319
Kyree/Ky <114588807+TheOfficialKyree@users.noreply.github.com>
2023-12-11 12:03:20 +0000

    Attempted to make multiplayer but one version wasn't working so i had to make a part 2 just in case

---
+++ Assets/Main Achievers Folder/Multiplayer(Attempt 1)/Multiplayer(Attempt 2)/PlayerConfigurationManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerConfigurationManager : MonoBehaviour
+{
+    private List<PlayerConfiguration> playerConfigs;
+
+    [SerializeField]
+    private int MaxPlayers = 2;
+
+    public static PlayerConfigurationManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.Log("Singleton- Trying to create another instance of singleton");
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+            playerConfigs = new List<PlayerConfiguration>();
+
+        }
+    }
+
+    public void SetPlayerColor(int index, Material color)
+    {
+        playerConfigs[index].PlayerMaterial = color;
+    }
+
+    public void ReadyPlayer(int index)
+    {
+        playerConfigs[index].IsReady = true;
+        if(playerConfigs.Count == MaxPlayers && playerConfigs.All(p => p.IsReady == true))
+        {
+
+        }
+    }
+}
+
+public class PlayerConfiguration
+{
+    public PlayerConfiguration(PlayerInput pi)
+    {
+        PlayerIndex = pi.playerIndex;
+        Input = pi;
+    }
+    public PlayerInput Input { get; set; }
+
+    public int PlayerIndex { get; set; }
+    public bool IsReady { get; set; }
+
+    public Material PlayerMaterial { get; set; }
+}
